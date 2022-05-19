2f8405e51de5c598780d0ac153c46e616efff92f
gitbmvb <git@github.com>
2022-05-19 00:15:29 -0300

    Starting models

--- administrator/models.py
+++ administrator/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    pass
+
+
+class Meet(models.Model):
+    about = models.TextField(blank=False, max_length=500)
+    biography = models.TextField(blank=False, max_length=1200)
+    curriculum = models.FileField(blank=True, upload_to="")
+    profilePicture = models.ImageField(blank=False, upload_to="")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Research(models.Model):
+    visibilityChoices = (
+        ("v", "Visible"),
+        ("i", "Invisible"),
+    )
+
+    description = models.TextField(blank=False, max_length=1200)
+    image = models.ImageField(blank=False, upload_to="")
+    image_subtitle = models.CharField(blank=False, max_length=200)
+    title = models.CharField(blank=False, max_length=200)
+    visibility = models.CharField(blank=False, choices=visibilityChoices, max_length=1)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Team(models.Model):
+    memberChoices = (
+        ("Mas", "Master Student"),
+        ("PhD", "PhD Student"),
+        ("Pos", "Post-doctoral fellow"),
+        ("PrI", "Principal Investigator"),
+        ("Und", "Undergraduate Student"),
+        ("Spe", "Specialist"),
+        ("Tec", "Technician"),
+    )
+    statusChoices = (
+        ("ac", "Active Member"),
+        ("al", "Alumni"),
+    )
+    email = models.EmailField(blank=False, max_length=100)
+    fullName = models.CharField(blank=False, max_length=100)
+    lattes = models.CharField(blank=False, max_length=100)
+    memberType = models.CharField(blank=False, choices=memberChoices, max_length=3)
+    profilePicture = models.ImageField(blank=False, upload_to="")
+    status = models.CharField(blank=False, choices=statusChoices, max_length=2)
+
+
+class Publications(models.Model):
+    authors = models.CharField(blank=False, max_length=100)
+    title = models.CharField(blank=False, max_length=100)
+    date = models.DateField(blank=False, )
+    journal = models.CharField(blank=False, max_length=100)
+    doi = models.URLField(blank=False, max_length=1000)
+    pmid = models.IntegerField(blank=False, max_length=10)
+
+
+
+
+
+
+
+
+
