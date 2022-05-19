cafb685ff5c510468e285a54164455fa008ff50c
gitbmvb <git@github.com>
2022-05-18 21:02:04 -0300

    Updated main.views, main.urls; and created the basic templates

--- main/views.py
+++ main/views.py
@@ -1,7 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 
-def index(request):
-    return render(request, "main/index.html", {})
+class IndexView(TemplateView):
+    template_name = "main/index.html"
+
+
+class MeetView(TemplateView):
+    template_name = "main/meet.html"
+
+
+class ResearchView(TemplateView):
+    template_name = "main/research.html"
+
+
+class TeamView(TemplateView):
+    template_name = "main/team.html"
+
+
+class PublicationsView(TemplateView):
+    template_name = "main/publications.html"
+
+
+class NewsView(TemplateView):
+    template_name = "main/news.html"
+
+
+class OpportunitiesView(TemplateView):
+    template_name = "main/opportunities.html"
+
+
+class DonateView(TemplateView):
+    template_name = "main/donate.html"
+
+
+class DirectionsView(TemplateView):
+    template_name = "main/directions.html"
 
